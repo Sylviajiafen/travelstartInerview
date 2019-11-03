658ea442d3a9f119f7e107c7a96c9c4a437defc9
Sylvia Jia Fen <kokoro.sharing@gmail.com>
2019-11-04 00:17:25 +0800

    clean codes

--- TravelstartInterview/Model/FileSeparator.swift
+++ TravelstartInterview/Model/FileSeparator.swift
@@ -47,11 +47,11 @@ class FileSeparator {
         
         dataArr.append(CellData(title: "景點位置", content: data.address))
         
-        dataArr.append(CellData(title: "交通資訊", content: data.info ?? ""))
+        dataArr.append(CellData(title: "交通資訊", content: data.info ?? "（無資訊）"))
         
-        dataArr.append(CellData(title: "附近捷運", content: data.mrt ?? ""))
+        dataArr.append(CellData(title: "附近捷運", content: data.mrt ?? "（無資訊）"))
         
-        dataArr.append(CellData(title: "開放時間", content: data.time ?? ""))
+        dataArr.append(CellData(title: "開放時間", content: data.time ?? "（無資訊）"))
         
         return dataArr
     }
@@ -63,5 +63,3 @@ struct CellData {
     
     let content: String
 }
-
-//let titles: [String] = ["景點名稱", "景點介紹", "景點位置", "交通資訊", "附近捷運", "開放時間"]
--- TravelstartInterview/View/Detail/DetailTableViewCell.swift
+++ TravelstartInterview/View/Detail/DetailTableViewCell.swift
@@ -14,52 +14,13 @@ class DetailTableViewCell: UITableViewCell {
     
     @IBOutlet weak var content: UITextView!
     
-    let titles: [String] = ["景點名稱", "景點介紹", "景點位置", "交通資訊", "附近捷運", "開放時間"]
-    
     func layout(by data: CellData) {
         
         title.text = data.title
         
         content.text = data.content
     }
-    
-    func layout(at index: Int, by data: Results) {
-        
-        print(index)
-        
-        title.text = titles[index]
-        
-        switch index {
-            
-        case 0:
-            
-            content.text = data.title
-            
-        case 1:
-            
-            content.text = data.description
-            
-        case 2:
-            
-            content.text = data.address
-            
-        case 3:
-            
-            content.text = data.info ?? ""
-            
-        case 4:
-            
-            content.text = data.mrt ?? ""
-            
-        case 5:
-            
-            content.text = data.time ?? ""
-    
-        default:
-            break
-        }
-    }
-    
+
     override func awakeFromNib() {
         super.awakeFromNib()
     }
